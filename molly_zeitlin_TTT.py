9fb70cf87043ce51c6a14c2ad75cbb06e8e818c5
molly-zeitlin <mzeitlin28@gcds.net>
2026-03-27 11:13:30 -0400

    Add files via upload

--- molly_zeitlin_TTT.py
+++ molly_zeitlin_TTT.py
@@ -4,14 +4,27 @@
 import random
 
 def display_board(board):
-    """Print the board in a user-friendly format."""
-    for row in board:
-        for cell in row:
-            print(cell, end=' ')
+    '''Print the board in a user-friendly format.
+    Args:
+        board: 2 dimensional array
+    Returns:
+        print: the board in a user-friendly format
+    '''
+    for row in board:                                                                                           #create for loop to isolate each row
+        for cell in row:                                                                                        #create another for loop to isolate each cell within each row
+            print(cell, end=' ')                                                                                #display cell
         print()
 
 def get_move(board, player, xo):
-    '''Get valid row and column for computer or player move.'''
+    '''Get valid row and column for computer or player move.
+    Args:
+        board: 2 dimensional array
+        player(str): either user or comp
+        xo: either x or o depending on who's move
+    Returns:
+        the altered board
+        print: message that the user's input was incorrect
+    '''
     while True:
         if player == "comp":                                                                                    #if it is computer's turn
             move = random.randint(1, 9)                                                                         #set variable move to random number between 1 and 9 (inclusive)
@@ -22,12 +35,13 @@ def get_move(board, player, xo):
                 print("INVALID RESPONSE\nplease enter valid number")                                            #display message
                 continue                                                                                        #restart while loop
 
-        if move == 1:                                                                                           #if variable move is set to 1
-            if board[0][0] == "| 1 |":                                                                          #if 1 spot is available
-                board[0][0] = (f"| {xo} |")                                                                     #set 1 spot to x or o (based on which player's turn it is)
-                break                                                                                           #end loop
-            elif player == "user":                                                                              #if 1 spot is unavaible and it is a user's turn
-                print("That space is already taken: please enter valid space")                                  #display message
+        #if variable is set to 1-9, check if that spot is avaible and set that spot to x or o (based on which player's turn it is) and end loop. if spot is unavailable, then check if it is the user's turn and display message
+        if move == 1:                                                                                           
+            if board[0][0] == "| 1 |":                                                                          
+                board[0][0] = (f"| {xo} |")                                                                    
+                break                                                                                   
+            elif player == "user":                                                                         
+                print("That space is already taken: please enter valid space")                       
         elif move == 2:
             if board[0][1] == "| 2 |":
                 board[0][1] = (f"| {xo} |")
@@ -81,7 +95,13 @@ def get_move(board, player, xo):
     return board
 
 def check_winner(board):
-    """Determine who winner is (if there is one)."""
+    '''Determine who winner is (if there is one).
+    Args:
+        board: 2 dimensional array
+    Returns:
+        print: if there is a winner, displays who won
+    '''
+    #if three spots on board in a row are the same (either x or o), then display message saying that player has won and end game
     if "| x |" == board[0][0] == board[0][1] == board[0][2] or "| x |" == board[0][0] == board[1][0] == board[2][0] or "| x |" == board[0][0] == board[1][1] == board[2][2] or "| x |" == board[0][2] == board[1][1] == board[2][0] or "| x |" == board[0][1] == board[1][1] == board[2][1] or "| x |" == board[0][2] == board[1][2] == board[2][2] or "| x |" == board[1][2] == board[1][1] == board[1][0] or "| x |" == board[2][0] == board[2][1] == board[2][2]:
         print("Player x has won the game!\nGAME OVER!")
         quit()
@@ -90,7 +110,7 @@ def check_winner(board):
         quit()
 
 def play_game():
-    """Run one complete game of Tic-Tac-Toe."""
+    '''Run one complete game of Tic-Tac-Toe.'''
     turns = 0
     while True:
         board= [                                                            #build 2-dimensional board
@@ -100,23 +120,23 @@ def play_game():
         ]
         display_board(board)
         print("Welcome to Tic-Tac-Toe!")
-        uc1 = input("Would you like to play against the computer or in 2 player mode? (Write C or 2): ")
+        #ask user questions until game play begins
+        uc1 = input("Would you like to play against the computer or in 2 player mode? (Write C or 2): ") #set variable (user choice 1) to user's response 
         if uc1 == "C" or uc1 == "c":
             while True:
-                uc2 = input("Who would you like to be? (Enter X or O): ")
-                if uc2 == "X" or uc2 == "x":
-                    user = "x"
+                user = input("Who would you like to be? (Enter X or O): ").lower()
+                if user == "x":
                     computer = "o"
                     break
-                elif uc2 == "O" or uc2 == "o":
-                    user = "o"
+                elif user == "o":
                     computer = "x"
                     break
                 else:
                     print("INVALID RESPONSE")
             while True:
-                uc3 = input("Would you like to go first? (Enter y or n): ")
-                if uc3 == "y" or uc3 == "Y":
+                uc3 = input("Would you like to go first? (Enter y or n): ").lower()
+                if uc3 == "y":
+                    #active game until draw or someone wins
                     while turns < 9:
                         board = get_move(board, "user", user)
                         display_board(board)
@@ -131,7 +151,8 @@ def play_game():
                         turns +=1
                     print("GAME OVER!\nNo one wins...\nTIE GAME!")
                     break
-                elif uc3 == "n" or uc3 == "N":
+                elif uc3 == "n":
+                    #active game until draw or someone wins
                     while turns < 9:
                         print("Computer's turn:")
                         board = get_move(board, "comp", computer)    
@@ -151,8 +172,9 @@ def play_game():
             break
         elif uc1 == "2":
             while True:
-                uc2 = input("Who would like to go first? (Enter X or O): ")
-                if uc2 == "x" or uc2 == "X":
+                uc2 = input("Who would like to go first? (Enter X or O): ").lower()
+                if uc2 == "x":
+                    #active game until draw or someone wins
                     while turns < 9:
                         board = get_move(board, "user", "x")
                         display_board(board)
@@ -166,7 +188,8 @@ def play_game():
                         turns +=1
                     print("GAME OVER!\nNo one wins...\nTIE GAME!")
                     break
-                elif uc2 == "O" or uc2 == "o":
+                elif uc2 == "o":
+                    #active game until draw or someone wins
                     while turns < 9:
                         board = get_move(board, "user", "o")
                         display_board(board)
