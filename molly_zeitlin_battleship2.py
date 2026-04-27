252adb34636e424b5a3422c5f3ca9b2b35f53d77
molly-zeitlin <mzeitlin28@gcds.net>
2026-04-27 14:35:10 -0400

    Add files via upload

--- molly_zeitlin_battleship2.py
+++ molly_zeitlin_battleship2.py
@@ -11,7 +11,7 @@ import os
 
 def display_board(board, player):
     '''
-    Print the board in a user-friendly format.
+    Description: Print the board in a user-friendly format.
     Args:
         board: 2 dimensional array
     Returns: void
@@ -24,13 +24,14 @@ def display_board(board, player):
 
 def choose_ships(secret_board):
     '''
-    Randomly choose 4 points on the secret board as the ships.
+    Description: Randomly choose 4 points on the secret board as the ships.
     Args:
         secret_board: 2 dimensional array
     Returns:
         secret_board: 2 dimensional array with ships chosen
     '''
     i = 0
+    #a loop to randomly place ships across the hidden board 4 times (4 ships in total on a board)
     while i < 4:
         row = random.randint(0, 4)
         column = random.randint(0, 4)
@@ -44,13 +45,14 @@ def choose_ships(secret_board):
 
 def get_coordinates(board, player):
     '''
-    Ask user for coodinate point for their next move.
+    Description: Ask user for coodinate point for their next move.
     Args:
         board: 2 dimensional array
     Returns:
         row: row where user wants to go for next move
         column: column where user wants to go for next mvove
     '''
+    #asks the user which row and which column they want to shoot at on their turn, and checks that the specified coordinate point is valid
     while True:
         try:
             row = int(input(f"{player}, what row would you like to shoot at? ")) - 1
@@ -74,7 +76,7 @@ def get_coordinates(board, player):
         
 def player_move(board, secret_board, row,column, player):
     '''
-    Shoots at user's desired point, and determines whether player "hit" or "missed" a ship.
+    Description: Shoots at user's desired point, and determines whether player "hit" or "missed" a ship.
     Args:
         board: 2 dimensional array
         secret_board: 2 dimensional array with ships chosen
@@ -83,6 +85,7 @@ def player_move(board, secret_board, row,column, player):
     Returns:
         board: 2 dimensional array (altered with "hits" or "misses")
     '''
+    #checks whether the user has hit a ship or missed, and changes the point on their board to reflect that result
     if secret_board[row][column] == "| s |":
         board[row][column] = "| H |"
         print(f"{player}, you hit a ship!!")
@@ -92,13 +95,13 @@ def player_move(board, secret_board, row,column, player):
 
 def check_win(board):
     '''
-    Find all "| H |"s in board and determine user has won if 4 "| H |" found
+    Description: Find all "| H |"s in board and determine user has won if 4 "| H |" found
     Args:
         board: 2 dimensional array (has user's "hits" and "misses")
     Returns:
         True: if user has won the game, (found all 4 ships) function returns True
     '''
-    ship = 0
+    ship = 0                                            #counts the number of ships found on the user's board                                             
     for r in range (5):
         for c in range (5):
             if board[r][c] == ("| H |"):
@@ -108,7 +111,7 @@ def check_win(board):
 
 def clear_screen(seconds):
     '''
-    Clears the terminal to clean up user's area
+    Description: Clears the terminal to clean up user's area
     Args:
         seconds: the amount of time the computer waits(in seconds) before clearing the screen
     Returns: void
@@ -118,28 +121,29 @@ def clear_screen(seconds):
 
 def main():
     #build the boards (2 dimensional arrays)
-    board1=[                                                           
+    
+    board1=[                                #this is player 1's shooting board (that they see with their hits and misses)                           
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |']
         ]
-    secret_board1=[                                                  
+    secret_board1=[                         #this is player 1's secret board (hidden but their ships are placed and stored in proper location on this board)                         
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |']
         ]
-    board2= [                                                         
+    board2= [                               #this is player 2's shooting board (that they see with their hits and misses)                              
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |']
         ]
-    secret_board2= [                                                    
+    secret_board2= [                        #this is player 2's secret board (hidden but their ships are placed and stored in proper location on this board)                            
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
         ['| x |', '| x |', '| x |', '| x |', '| x |'],
@@ -147,11 +151,11 @@ def main():
         ['| x |', '| x |', '| x |', '| x |', '| x |']
         ]
     print("Welcome to Battleship!\nEach player has 10 turns to find the other player's 4 hidden ships\nGOOD LUCK...")
-    secret_board1= choose_ships(secret_board1)
-    secret_board2 = choose_ships(secret_board2)
+    secret_board1= choose_ships(secret_board1)                              #places player 1's ships onto their hidden board
+    secret_board2 = choose_ships(secret_board2)                             #places player 2' ships onto their hidden board
     player1 = input("Who would like to go first? (Enter name) ")
     player2 = input("Who is going second? (Enter name) ")
-    turns = 0
+    turns = 0                                                               #variable is used to track how many times each player has shot at the board
     #loops of game play (each player goes 10 times unless someone wins)
     while turns<10: 
         display_board(board1,player1)
